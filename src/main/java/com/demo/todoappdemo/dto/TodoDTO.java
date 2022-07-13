83effc2765891de9205d2a4a1696ce04c6cdf79d
Aswad Abbas <aabbas@stellatechnology.com>
2022-07-13 13:03:54 +0500

    Map Struct Issues Fixed

--- src/main/java/com/demo/todoappdemo/dto/TodoDTO.java
+++ src/main/java/com/demo/todoappdemo/dto/TodoDTO.java
@@ -1,9 +1,13 @@
 package com.demo.todoappdemo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
+@Builder
 public class TodoDTO {
     private String contents;
     private int id;
--- src/main/java/com/demo/todoappdemo/dto/TodoListDTO.java
+++ src/main/java/com/demo/todoappdemo/dto/TodoListDTO.java
@@ -2,10 +2,14 @@ package com.demo.todoappdemo.dto;
 
 
 import com.demo.todoappdemo.entity.Todo;
+import lombok.Builder;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@Builder
 public class TodoListDTO {
     private int id;
     private String title;
